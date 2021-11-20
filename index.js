8dc65b1d01850bff07d8895060fa94e891ab87c0
simonachess <simonachess@gmail.com>
2021-11-20 18:01:47 +0200

    initial solution

---
+++ index.js
@@ -0,0 +1,18 @@
+function textAreaSize(minRows, maxRows) {
+    const textArea = document.getElementById('textarea');
+    if (textArea.value === '') {
+        textArea.setAttribute("rows", minRows);
+    }
+    textArea.addEventListener('input', () => {
+        let currentRows = textArea.value.split("\n").length;
+        if (currentRows <= minRows) {
+            textArea.setAttribute("rows", minRows);
+        }
+        else if (currentRows < maxRows) {
+            textArea.setAttribute("rows", currentRows);
+            currentRows++;
+        }
+    })
+}
+
+textAreaSize(2, 8);
